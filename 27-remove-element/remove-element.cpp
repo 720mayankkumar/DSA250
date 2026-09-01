72db1f4edf982d3ada436edf3a3d97d4aee929c5
MAYANK KUMAR <720mayankkumar@gmail.com>
2026-09-01 22:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (84.45%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,31 @@
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         int c = 0;
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] != val) {
+//                 nums[c] = nums[i];
+//                 c++;
+//             }
+//         }
+//         return c;
+//     }
+// };
+
+// approch -2
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int c = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != val) {
-                nums[c] = nums[i];
-                c++;
+        int n = nums.size();
+        int i = 0;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] =nums[n - 1];
+                n--;
+            } else {
+                i++;
             }
         }
-        return c;
+        return n;
     }
 };
