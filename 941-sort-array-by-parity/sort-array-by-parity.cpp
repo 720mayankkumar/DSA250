744545400a0a98563b747b6f7c77810ea188338f
MAYANK KUMAR <720mayankkumar@gmail.com>
2026-09-05 12:39:38 +0530

    Time: 5 ms (3.94%) | Memory: 20.6 MB (52.62%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,17 +1,32 @@
+// class Solution {
+// public:
+//     vector<int> sortArrayByParity(vector<int>& nums) {
+//         vector<int>ans;
+//         vector<int>temp;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]%2==0){
+//                 ans.push_back(nums[i]);
+//             }
+//             else temp.push_back(nums[i]);
+//         }
+//         for(int i=0;i<temp.size();i++){
+//             ans.push_back(temp[i]);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int>ans;
-        vector<int>temp;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0){
-                ans.push_back(nums[i]);
-            }
-            else temp.push_back(nums[i]);
+        vector<int> arr;
+        int n = nums.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] % 2 == 0) {
+                arr.insert(arr.begin(), nums[i]);
+            } else
+                arr.push_back(nums[i]);
         }
-        for(int i=0;i<temp.size();i++){
-            ans.push_back(temp[i]);
-        }
-        return ans;
+        return arr;
     }
 };
