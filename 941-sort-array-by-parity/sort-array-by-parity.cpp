dd2d96966b4ddf79ddf389a9a9acd5393446edfc
MAYANK KUMAR <720mayankkumar@gmail.com>
2026-09-05 12:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (79.79%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -16,17 +16,31 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     vector<int> sortArrayByParity(vector<int>& nums) {
+//         vector<int> arr;
+//         int n = nums.size();
+//         for (int i = n - 1; i >= 0; i--) {
+//             if (nums[i] % 2 == 0) {
+//                 arr.insert(arr.begin(), nums[i]);
+//             } else
+//                 arr.push_back(nums[i]);
+//         }
+//         return arr;
+//     }
+// };
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> arr;
-        int n = nums.size();
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] % 2 == 0) {
-                arr.insert(arr.begin(), nums[i]);
-            } else
-                arr.push_back(nums[i]);
+        int s = 0, e = nums.size() - 1;
+        while (s < e) {
+            if (nums[s] % 2 > nums[e] % 2) {
+                swap(nums[s], nums[e]);
+            }
+            if (nums[s] % 2 == 0) s++;
+            if (nums[e] % 2 == 1) e--;
         }
-        return arr;
+        return nums;
     }
 };
